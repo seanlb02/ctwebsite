231a255cee024e1ae0c03500545bc1091ac8eaed
Sean <12708@coderacademy.edu.au>
2023-03-08 17:41:30 +1100

    adding body

--- components/Homebody.js
+++ components/Homebody.js
@@ -11,7 +11,11 @@ export default function Homebody () {
                     <div className={styles.content}>hey</div>
             </section>
             <section className={styles.rightScroll}>
-                    ho
+                    <a target='_blank' href='https://xlr8r.com/mp3/loicc-der-nacht/'><Image src='/loiccXLR8R.png' height={370} width={900}></Image></a>
+                    <Image className={styles.image} src='/gutnosePhoto.jpeg' height={400} width={800}></Image>
+                    <a target='_blank' href='https://ra.co/reviews/18312'><Image  src='/2200RA.png' height={350} width={1100}></Image></a>
+                    <div>hey</div>
+                    <div>hey</div>
             </section>
 
 
